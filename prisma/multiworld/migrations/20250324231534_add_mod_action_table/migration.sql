a7b401a66057af7777cc36ecc64e8c9db6f6b840
breakcraft <7675056+breakcraft@users.noreply.github.com>
2025-04-02 22:22:14 -0400

    fix: update .gitignore and enhance VSCode settings for SQL connections

--- prisma/multiworld/migrations/20250324231534_add_mod_action_table/migration.sql
+++ prisma/multiworld/migrations/20250324231534_add_mod_action_table/migration.sql
@@ -7,6 +7,5 @@ CREATE TABLE `mod_action` (
     `data` VARCHAR(191) NULL,
     `ip` VARCHAR(191) NULL,
     `timestamp` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
